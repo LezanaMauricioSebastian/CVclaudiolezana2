9217caec2739033ff6f8e089a14fca6d65badef2
mauri2012 <lezanamauricio86@gmail.com>
2023-07-18 21:44:49 -0300

    idoma arreglado

--- assets/js/idiomas.js
+++ assets/js/idiomas.js
@@ -3,12 +3,14 @@ let selectIdioma = document.querySelector('#idiomas');
 
 selectIdioma.addEventListener('change',language);
 console.log(selectIdioma.value);
+console.log(window.location.pathname);
 function language(){
     option=selectIdioma.value;    
     if(option==2){
         location.href="../index.html";
     }else{
-        location.href="en/index.html";
+        
+            location.href="en/index.html";
         
     }
 }
